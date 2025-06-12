4d91b76388c3877dbad092669ab70235ca28b502
Anna <annathegoodgirl@protonmail.com>
2025-06-12 06:18:11 -0500

    Add files via upload

---
+++ python module installer.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Please enter the name of the Python module you want to install:
+set /p moduleName=
+
+:: Set the path to the Python executable and Scripts directory
+set PYTHON_PATH=C:\Users\joshclark756\AppData\Local\Programs\Python\Python313
+set SCRIPTS_PATH=%PYTHON_PATH%\Scripts
+
+:: Add Python and Scripts directories to PATH
+set PATH=%PYTHON_PATH%;%SCRIPTS_PATH%;%PATH%
+
+:: Install the module
+pip install %moduleName%
+
+pause
